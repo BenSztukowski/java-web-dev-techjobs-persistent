a069cb22c1aa2dff27bfcc779bd1ef4d477605e5
Ben <bensztukowski@gmail.com>
2021-06-25 10:16:14 -0500

    Finished assignment

--- queries.sql
+++ queries.sql
@@ -5,3 +5,6 @@ SELECT name FROM employers WHERE location = "St. Louis City";
 ## Part 3: Test it with SQL
 DROP TABLE job;
 ## Part 4: Test it with SQL
+SELECT distinct skill.name,skill.description,job.name FROM job
+join job_skills on job.id= job_skills.jobs_id
+join skill on skill.id = job_skills.skills_id
